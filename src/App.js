97814df4974452d33f98fb14f36698d2262b4b4b
Tharun Pirangi <pirangitharun2000@gmail.com>
2023-01-13 15:31:54 +0530

    style changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import React, {useEffect, useState} from 'react';
 import './App.css';
+import partyPopper from './partyPopper.png';
 
 function App() {
   const secretWords = ['knobs', 'venom','sword','vague','yatch','blink','mocha','binge','pulse','whale','viper','lemon','olive','hydra','horse'];
   const [randomWord, setRandomWord] = useState('');
   const [success, setSuccess] = useState(false);
+  const [showHint, setShowHint] = useState(false);
   useEffect(()=>{
     setRandomWord(secretWords[Math.floor(Math.random()*secretWords.length)]);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   
   const [words, setWords] = useState([]);
-  let temp = '';
+  const [entered, setEntered] = useState('');
   const handleChange = (e) =>{
-    temp = e.target.value;
+    setEntered(e.target.value);
   }
   const handleClick = () =>{
-    let count = compareWords(randomWord,temp);
-    setSuccess(temp === randomWord);
-    setWords(prev=> [...prev, {"name": temp,"count": count}]);
+    let count = compareWords(randomWord,entered);
+    setSuccess(entered === randomWord);
+    if(entered.length===5) setWords(prev=> [...prev, {"name": entered,"count": count}]);
+    if(entered===randomWord){
+      document.body.style.backgroundImage = `url(${partyPopper})`;
+    }
+    setEntered('');
   }
   const compareWords = (s1, s2) => {
     let count = 0;
@@ -28,21 +35,44 @@ function App() {
     }
     return count;
   }
+  const getHintWord = () =>{
+    return randomWord.charAt(0)+ " _ "+randomWord.charAt(2)+" _  "+randomWord.charAt(4);
+  }
   return (
-    <div>
+    <div id='parentDiv' className='container'>
       <h1 className='heading'>Guess the Secret Word</h1>
+      <p className='inputs' style={{marginBottom: '2rem'}}>Enter the 5 letter word with distinct letters. You can access hint after 3 unsuccessful attempts.</p>
       {/* <h2 style={{display:'flex', justifyContent: 'center'}}>Random Word is {randomWord}</h2> */}
       <div className='inputs'>
-        <input className='inputBox' type="text" onChange={handleChange} />
+        <input id="input-word" className='inputBox' type="text" value={entered} onChange={handleChange} />
         <button className='submitBtn' type="submit" onClick={handleClick}>Submit</button>
       </div>
+      {words.length>=3 && !success && <div className='hintBox'>
+        <button className='hintBtn' onClick={()=>setShowHint(!showHint)}>See Hint</button>
+        {showHint && <p className='hintWord'>{getHintWord()}</p>}
+      </div>}
+      
       {success? <div className='successBox'>
-        <h2 style={{display:'flex', justifyContent: 'center'}}>Congratulations!!!</h2>
-        <p style={{display:'flex', justifyContent: 'center'}}>You guessed it within {words.length} attempts</p>
+        <h2 className='inputs'>Congratulations!!!</h2>
+        <p className='inputs'>You guessed it within {words.length} attempts</p>
       </div>:
-      words.map((item,index)=>(
-        <h4 key={index} style={{display:'flex', justifyContent: 'center'}}>{item.name} {item.count}</h4>
+      words.length? <table>
+        <thead>
+      <tr>
+        <th>Guess</th>
+        <th># of Matching letters</th>
+      </tr>
+      </thead>
+      <tbody>
+      {words.map((item,index)=>(
+        <tr key={index}>
+          <td>{item.name}</td>
+          <td>{item.count}</td>
+        </tr>
       ))}
+      </tbody>
+    </table>: <></>
+    }
     </div>
   );
 }
